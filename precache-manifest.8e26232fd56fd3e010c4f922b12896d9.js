d8dcfc6cd46d2f53028d0df59917f98af70d0d58
juanf1328 <juanf1328@gmail.com>
2022-03-28 15:59:22 -0300

    Updates

---
+++ precache-manifest.8e26232fd56fd3e010c4f922b12896d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1872f68d08364d603ad8",
+    "url": "/RoboFriendsApp/static/css/main.03f301fd.chunk.css"
+  },
+  {
+    "revision": "1872f68d08364d603ad8",
+    "url": "/RoboFriendsApp/static/js/main.d198b4cb.chunk.js"
+  },
+  {
+    "revision": "9aa74f3c1fd7d892e3ab",
+    "url": "/RoboFriendsApp/static/js/runtime~main.4f498d75.js"
+  },
+  {
+    "revision": "d78164ce92e8439f4ed2",
+    "url": "/RoboFriendsApp/static/css/2.600cf690.chunk.css"
+  },
+  {
+    "revision": "d78164ce92e8439f4ed2",
+    "url": "/RoboFriendsApp/static/js/2.a3680423.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/RoboFriendsApp/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "4881b794778f93c3e67c6320be95e043",
+    "url": "/RoboFriendsApp/static/media/Transformers.4881b794.TTF"
+  },
+  {
+    "revision": "3fe0d905e1c545622c4131b82b73baa9",
+    "url": "/RoboFriendsApp/index.html"
+  }
+];
